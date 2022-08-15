98e7baddfcd05adbe89502d329e0f3c2a38d0c94
mikemakohon <mike.makogon@gmail.com>
2022-08-15 13:04:08 +0300

    fix: remove unnecessary import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
 
 const root = ReactDOM.createRoot(
